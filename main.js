5493d73673017cfdec3310cdf0c3e1c0966f9f53
VladimirSh6892 <vladimirsh6892@gmail.com>
2021-06-02 15:41:44 +0300

    add function createPlayer

--- main.js
+++ main.js
@@ -17,3 +17,37 @@ const player2 = {
         console.log(this.name + 'Fight');
     },
 };
+
+function createPlayer(name, obj) {
+    const $arenas = document.querySelector('.arenas');
+    const $player = document.createElement('div');
+    $player.classList.add(name);
+
+    const $progressbar = document.createElement('div');
+    $progressbar.classList.add('progressbar');
+
+    const $life = document.createElement('div');
+    $life.classList.add('life');
+    $life.style.width = `${obj.hp}%`;
+    $progressbar.appendChild($life);
+
+    const $name = document.createElement('div');
+    $name.classList.add('name');
+    $name.innerHTML = obj.name;
+    $progressbar.appendChild($name);
+
+    $player.appendChild($progressbar);
+
+    const $character = document.createElement('div');
+    $character.classList.add('character');
+    const $img = document.createElement('img');
+    $img.src = obj.img;
+    $character.appendChild($img);
+
+    $player.appendChild($character);
+    $arenas.appendChild($player);
+
+};
+
+createPlayer('player1', player1);
+createPlayer('player2', player2);
